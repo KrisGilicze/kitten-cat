21fdfac06e2935116a4dea8902f3bfc579a635c0
Kristof Gilicze <kristof.gilicze@horizon-alpha.com>
2020-09-07 18:17:18 +0200

    setting up flow

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the UI Kitten template
- * https://github.com/akveo/react-native-ui-kitten
- *
- * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
- *
- * @format
- */
-
-import React from 'react';
-import { StyleSheet } from 'react-native';
+// @flow
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {
   ApplicationProvider,
   Button,
@@ -20,39 +9,38 @@ import {
   Layout,
   Text,
 } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
 /**
  * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
  * https://akveo.github.io/eva-icons
  */
-const HeartIcon = (props) => (
-  <Icon {...props} name='heart'/>
-);
+const HeartIcon = (props) => <Icon {...props} name="heart" />;
 
-export default () => (
-  <>
-    <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category='h1'>
-          Welcome to UI Kitten 😻
-        </Text>
-        <Text style={styles.text} category='s1'>
-          Start with editing App.js to configure your App
-        </Text>
-        <Text style={styles.text} appearance='hint'>
-          For example, try changing theme to Dark by using eva.dark
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-          LIKE
-        </Button>
-      </Layout>
-    </ApplicationProvider>
-  </>
-);
-
+export default function App() {
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <Layout style={styles.container}>
+          <Text style={styles.text} category="h1">
+            Welcome to UI Kitten 😻
+          </Text>
+          <Text style={styles.text} category="s1">
+            Start with editing App.js to configure your App
+          </Text>
+          <Text style={styles.text} appearance="hint">
+            For example, try changing theme to Dark by using eva.dark
+          </Text>
+          <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
+            LIKE
+          </Button>
+        </Layout>
+      </ApplicationProvider>
+    </>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
