ffc4447daeb07005e702eaa5c023d0033a62e4c3
Kristof Gilicze <kristof.gilicze@horizon-alpha.com>
2020-09-08 13:03:58 +0200

    added navigation

--- App.js
+++ App.js
@@ -1,56 +1,14 @@
-// @flow
-import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {
-  ApplicationProvider,
-  Button,
-  Icon,
-  IconRegistry,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import React from 'react';
 import * as eva from '@eva-design/eva';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {AppNavigator} from './components/Navigator';
 
-/**
- * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
- * https://akveo.github.io/eva-icons
- */
-const HeartIcon = (props) => <Icon {...props} name="heart" />;
-
-export default function App(): React.Node {
-  return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.dark}>
-        <Layout style={styles.container}>
-          <Text style={styles.text} category="h1">
-            Welcome to UI Kitten 😻
-          </Text>
-          <Text style={styles.text} category="s1">
-            Start with editing App.js to configure your App
-          </Text>
-          <Text style={styles.text} appearance="hint">
-            For example, try changing theme to Dark by using eva.dark
-          </Text>
-          <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-            LIKE
-          </Button>
-        </Layout>
-      </ApplicationProvider>
-    </>
-  );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    textAlign: 'center',
-  },
-  likeButton: {
-    marginVertical: 16,
-  },
-});
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <AppNavigator />
+    </ApplicationProvider>
+  </>
+);
